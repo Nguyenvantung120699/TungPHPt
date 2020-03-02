e010ede420f72e09ab8e28eb30a4e0b8b5f36895
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-02 21:12:18 +0700

    klkl

--- resources/views/html/header.blade.php
+++ resources/views/html/header.blade.php
@@ -13,13 +13,13 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item active"><a class="nav-link" href="/TungPHPt/public/">Home</a></li>
+							<li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								 aria-expanded="false">Shop</a>
 								<ul class="dropdown-menu">
 									<li class="nav-item"><a class="nav-link" href="category.html">Shop Category</a></li>
-									<li class="nav-item"><a class="nav-link" href="/TungPHPt/public/productsdetails">Product Details</a></li>
+									<li class="nav-item"><a class="nav-link" href="/productsdetails">Product Details</a></li>
 									<li class="nav-item"><a class="nav-link" href="checkout.html">Product Checkout</a></li>
 									<li class="nav-item"><a class="nav-link" href="cart.html">Shopping Cart</a></li>
 									<li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
@@ -42,7 +42,7 @@
 									<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
 								</ul>
 							</li>
-							<li class="nav-item"><a class="nav-link" href="/TungPHPt/public/contacts">Contact</a></li>
+							<li class="nav-item"><a class="nav-link" href="/contacts">Contact</a></li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
 							<li class="nav-item"><a href="#" class="cart"><span class="ti-bag"></span></a></li>
