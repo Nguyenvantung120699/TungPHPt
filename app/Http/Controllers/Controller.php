bb8ebf1fb6e4b8c98e69945c76b39e06c011afb7
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-06 16:55:11 +0700

    kdkdksd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -31,23 +31,23 @@ class Controller extends BaseController
     // }
 
     public function home(){
-        //$products = Product::take(10)->orderBy('product_name','asc')->get(); // tra ve 1 collection voi moi phan tu la 1 object Product
-        $newests = Product::orderBy('created_at','desc')->take(10)->get();
-        $cheaps = Product::orderBy("price",'asc')->take(10)->get();
-        $exs = Product::orderBy('price','desc')->take(10)->get();
+        //$products = Products::take(10)->orderBy('product_name','asc')->get(); // tra ve 1 collection voi moi phan tu la 1 object Product
+        $newests = Products::orderBy('created_at','desc')->take(10)->get();
+        $cheaps = Products::orderBy("price",'asc')->take(10)->get();
+        $exs = Products::orderBy('price','desc')->take(10)->get();
         return view("home",['newests'=>$newests,'cheaps'=>$cheaps,'exs'=>$exs]);
     }
 
     public function productsdetails(){
         $product = Product::find(1);// tra ve 1 object Product theo id
    //     $category = Category::find($product->category_id);
-        $category_products = Product::where("category_id",$product->category_id)->where('id',"!=",$product->id)->take(10)->get();
-        $brand_products = Product::where("brand_id",$product->brand_id)->where('id',"!=",$product->id)->take(10)->get();
+        $category_products = Products::where("category_id",$product->category_id)->where('id',"!=",$product->id)->take(10)->get();
+        $brand_products = Products::where("brand_id",$product->brand_id)->where('id',"!=",$product->id)->take(10)->get();
         return view('productsDetails',['product'=>$product,'category_products'=>$category_products,'brand_products'=>$brand_products]);
     }
 
     public function shop($id){
-        $products = Product::where("category_id",$id)->take(20)->orderBy('created_at','desc')->get();// loc theo category
+        $products = Products::where("category_id",$id)->take(20)->orderBy('created_at','desc')->get();// loc theo category
         return view("shop",['products'=>$products]);
     }
 }
