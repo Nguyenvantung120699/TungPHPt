c08b713eba7f96e725954b8e366fc4f0ab72bc44
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-09 14:13:36 +0700

    sadsd

--- resources/views/html/header.blade.php
+++ resources/views/html/header.blade.php
@@ -3,7 +3,7 @@
 			<nav class="navbar navbar-expand-lg navbar-light main_box">
 				<div class="container">
 					<!-- Brand and toggle get grouped for better mobile display -->
-					<a class="navbar-brand logo_h" href="/TungPHPt/public/"><img src="img/logo.png" alt=""></a>
+					<a class="navbar-brand logo_h" href="{{asset("/TungPHPt/public/")}}"><img src="{{asset("img/logo.png")}}" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="icon-bar"></span>
@@ -13,7 +13,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 							<ul class="main-menu">
-							<li><a href="/">Home</a></li>
+							<li><a href=""{{url("/)}}">Home</a></li>
 							@foreach(\App\Category::all() as $c)
 							<li><a href="{{url("{$c->id}")}}">{{$c->category_name}}</a></li>
 							@endforeach
