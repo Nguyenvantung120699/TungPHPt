1b162681850a0efe6e30b41c84e966be6ddb50e9
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-10 23:04:29 +0700

    asdas

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,7 +1,26 @@
 @extends('layout')
 
+
+
 @section('content')
-				<!-- End Filter Bar -->
+
+<section class="banner-area organic-breadcrumb">
+		<div class="container">
+			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+				<div class="col-first">
+					<h1>Shop Category</h1>
+					<nav class="d-flex align-items-center">
+						<a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
+						<a href="#">Shop<span class="lnr lnr-arrow-right"></span></a>
+						<a href="single-product.html">product-category</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Banner Area -->
+
+	<!--================Single Product Area =================-->
 	<section class="lattest-product-area pb-40 category-list">
 		<h1>{{$category->category_name}}</h1>
 		<div class="row">
@@ -41,4 +60,7 @@
 				@endforeach
 			</div>
 		</section>
+	<!--================End Single Product Area =================-->
+
+
 @endsection
