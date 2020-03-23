f5e5fc03070be2cddf14ba460f6b867b61a0c302
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-23 16:09:28 +0700

    asa

--- resources/views/html/header.blade.php
+++ resources/views/html/header.blade.php
@@ -14,17 +14,7 @@
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
 							<li class="nav-item"><a class="nav-link" href="{{url("/")}}">Home</a></li>
-							<li class="nav-item submenu dropdown">
-								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-								 aria-expanded="false">Shop</a>
-								<ul class="dropdown-menu">
-									<li class="nav-item"><a class="nav-link" href="{{url("/shopproduct")}}">Shop Category</a></li>
-									<li class="nav-item"><a class="nav-link" href="{{url("/productsdetails")}}>Product Details</a></li>
-									<li class="nav-item"><a class="nav-link" href="">Product Checkout</a></li>
-									<li class="nav-item"><a class="nav-link" href="#">Shopping Cart</a></li>
-									<li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
-								</ul>
-							</li>
+							<li class="nav-item"><a class="nav-link" href="{{url("/shopproduct")}}">Shop</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								 aria-expanded="false">Blog</a>
--- resources/views/shopProduct.blade.php
+++ resources/views/shopProduct.blade.php
@@ -22,16 +22,6 @@
 		<!-- single product slide -->
 		<div class="single-product">
 			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-lg-6 text-center">
-						<div class="section-title">
-							<h1>Latest Products</h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore
-								magna aliqua.</p>
-						</div>
-					</div>
-				</div>
 				<div class="text-center">
 					<h3>Sản Phẩm Mới Nhất</h3>
 				</div>
