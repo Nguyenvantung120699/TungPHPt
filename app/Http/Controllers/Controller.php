bdad640cb06221d4a528a48c19cfe033fd604f6c
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-23 15:48:59 +0700

    aaa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\OrederCreate;
 use App\Products;
 use App\Category;
 use App\Order;
