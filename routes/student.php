faab553eaeb6ab7b682392767d538d746128c135
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-27 17:04:52 +0700

    asasas

--- routes/student.php
+++ routes/student.php
@@ -3,7 +3,7 @@ Route::get('student',"StudentController@student");
 Route::get('student/create',"StudentController@studentCreate");
 Route::post('student/store',"StudentController@studentStore");
 
-Route::get('student/edit/{id}',"AdminController@studentEdit");
-Route::post('student/update/{id}',"AdminController@studentUpdate");
+Route::get('student/edit/{id}',"StudentController@studentEdit");
+Route::post('student/update/{id}',"StudentController@studentUpdate");
 
-Route::get('student/delete/{id}',"AdminController@studentDestroy");
+Route::get('student/delete/{id}',"StudentController@studentDestroy");
