3bf0606024880a6db71640d6c792f905711f7d3a
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-27 16:59:20 +0700

    asasa

--- routes/student.php
+++ routes/student.php
@@ -2,3 +2,8 @@
 Route::get('student',"StudentController@student");
 Route::get('student/create',"StudentController@studentCreate");
 Route::post('student/store',"StudentController@studentStore");
+
+Route::get('student/edit/{id}',"AdminController@studentEdit");
+Route::post('student/update/{id}',"AdminController@studentUpdate");
+
+Route::get('student/delete/{id}',"AdminController@studentDestroy");
