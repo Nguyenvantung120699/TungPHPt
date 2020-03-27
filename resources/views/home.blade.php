e27edf1d1c4b9032680966af41c287935d1dd8be
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-27 20:02:02 +0700

    asas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -283,21 +283,21 @@
 									</div>
 									<div class="prd-bottom">
 
-										<a href="" class="social-info">
+										<a href="{{url("shopping/{$product->id}" class="social-info">
 											<span class="ti-bag"></span>
 											<p class="hover-text">add to bag</p>
 										</a>
-										<a href="" class="social-info">
+										<a href="{{url("shop/{$p->category_id}")}}" class="social-info">
 											<span class="lnr lnr-heart"></span>
-											<p class="hover-text">Wishlist</p>
+											<p class="hover-text">category more</p>
 										</a>
 										<a href="" class="social-info">
 											<span class="lnr lnr-sync"></span>
 											<p class="hover-text">compare</p>
 										</a>
-										<a href="{{url("shop/{$p->category_id}")}}" class="social-info">
+										<a href="{{url("productsdetails/{$p->id}")}}" class="social-info">
 											<span class="lnr lnr-move"></span>
-											<p class="hover-text">category more</p>
+											<p class="hover-text">view more</p>
 										</a>
 									</div>
 								</div>
